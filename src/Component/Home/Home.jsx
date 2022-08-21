9309c1e59c283bf1eac55211fa0e5fab894b6054
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-08-21 21:10:31 +0530

    TourismApp: Icon Change.

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Home.css";
-import ApartmentIcon from "@mui/icons-material/Apartment";
+import { MdApartment } from 'react-icons/md'
 const Home = () => {
   return (
     <div className="home">
       <div className="icon">
-        <ApartmentIcon />
+        <MdApartment size="3rem"/>
       </div>
       <h1>Visit New York</h1>
       <h3>Express yourself and explore</h3>

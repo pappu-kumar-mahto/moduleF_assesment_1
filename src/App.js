500ca1a9ac979d6db43b1e0643d4b66e987ff06a
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-08-21 13:47:26 +0530

    TourismApp: started.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './Component/Navbar/Navbar'
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;

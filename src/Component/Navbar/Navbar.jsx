8c3e2f192cbac760ab641a3b0e7f2c5ec64f8519
pappu-kumar-mahto <pappukumar.mahto@gmail.com>
2022-08-21 16:03:57 +0530

    TourismApp: Navbar Part completed.

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 const Navbar = () => {
   return (
-    <div>
-      navbar
+    <div className="navbar">
+      <div className="explore details">
+        <sapn>Explore</sapn>
+      </div>
+      <div className="navMenu">
+        <NavLink to="/home">
+          <span>Home</span>
+        </NavLink>
+        <NavLink to="/place">
+          <span>Place</span>
+        </NavLink>
+        <NavLink to="/video">
+          <span>Video</span>
+        </NavLink>
+        <NavLink to="/locations">
+          <span>Locations</span>
+        </NavLink>
+        <NavLink to="/about">
+          <span>About</span>
+        </NavLink>
+        <NavLink to="/book">
+          <span>Book</span>
+        </NavLink>
+        <NavLink to="/photo">
+          <span>Photos</span>
+        </NavLink>
+      </div>
+      <div className="learnMore details">
+        <span>Learn More</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
